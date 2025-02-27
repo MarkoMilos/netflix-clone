5036388f6502e6c9aec91a28d5fc94bff933dc2d
Marko Milos <marko.milos.hr@gmail.com>
2025-02-27 11:47:18 +0100

    Refactor ContentCard styles and markup; remove redundant dialog inner container and streamline dialog layout for improved readability and maintainability.

--- components/ContentCard/ContentCard.tsx
+++ components/ContentCard/ContentCard.tsx
@@ -24,51 +24,49 @@ export default function ContentCard({ item, dialogPosition = "align-center" }: C
       <div className={styles.title}>{item.contentTitle}</div>
 
       <div className={dialogClasses}>
-        <div className={styles.dialogInner}>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img className={styles.dialogPoster} src={item.contentPosterImage} alt="poster" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img className={styles.dialogPoster} src={item.contentPosterImage} alt="poster" />
 
-          <div className={styles.dialogContent}>
-            <div className={styles.dialogTitle}>{item.contentTitle}</div>
+        <div className={styles.dialogContent}>
+          <div className={styles.dialogTitle}>{item.contentTitle}</div>
 
-            <div className={styles.dialogActions}>
-              {/* Play button */}
-              <Link href={`/watch/${item.contentId}`}>
-                <div className={styles.playButton}>
-                  <Icon name="play" className={styles.playIcon} />
-                </div>
-              </Link>
+          <div className={styles.dialogActions}>
+            {/* Play button */}
+            <Link href={`/watch/${item.contentId}`}>
+              <div className={styles.playButton}>
+                <Icon name="play" className={styles.playIcon} />
+              </div>
+            </Link>
 
-              {/* Add to watchlist button */}
-              <button type="button" className={styles.watchlistButton}>
-                <Icon name="plus" className={styles.watchlistIcon} />
-              </button>
+            {/* Add to watchlist button */}
+            <button type="button" className={styles.watchlistButton}>
+              <Icon name="plus" className={styles.watchlistIcon} />
+            </button>
 
-              {/* Like button */}
-              <button type="button" className={styles.likeButton}>
-                <Icon name="like" className={styles.likeIcon} />
-              </button>
+            {/* Like button */}
+            <button type="button" className={styles.likeButton}>
+              <Icon name="like" className={styles.likeIcon} />
+            </button>
 
-              {/* More button */}
-              <button type="button" className={styles.moreButton}>
-                <Icon name="more" className={styles.moreIcon} />
-              </button>
-            </div>
+            {/* More button */}
+            <button type="button" className={styles.moreButton}>
+              <Icon name="more" className={styles.moreIcon} />
+            </button>
+          </div>
 
-            <div className={styles.dialogLabels}>
-              <span className={styles.dialogYear}>{item.contentYear}</span>
-              <span className={styles.dialogRating}>Rating: {item.contentRating}</span>
-              <Icon className={styles.spatialIcon} name="spatial" />
-            </div>
+          <div className={styles.dialogLabels}>
+            <span className={styles.dialogYear}>{item.contentYear}</span>
+            <span className={styles.dialogRating}>Rating: {item.contentRating}</span>
+            <Icon className={styles.spatialIcon} name="spatial" />
+          </div>
 
-            <div className={styles.dialogGenres}>
-              {["Action", "Adventure", "Suspense"].map((genre, index) => (
-                <span className={styles.genreLabel} key={genre}>
-                  {index > 0 && <span className={styles.genreSeparator}>&middot;</span>}
-                  {genre}
-                </span>
-              ))}
-            </div>
+          <div className={styles.dialogGenres}>
+            {["Action", "Adventure", "Suspense"].map((genre, index) => (
+              <span className={styles.genreLabel} key={genre}>
+                {index > 0 && <span className={styles.genreSeparator}>&middot;</span>}
+                {genre}
+              </span>
+            ))}
           </div>
         </div>
       </div>
